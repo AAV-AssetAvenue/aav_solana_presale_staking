43245d69fbd80ae778d12f45c0bbfd944254ad14
Asadullah khan <asadmuhammad427@gmail.com>
2025-03-05 21:03:18 +0500

    fix presale issue

--- scripts/transferOwnership.ts
+++ scripts/transferOwnership.ts
@@ -76,75 +76,25 @@ async function main() {
           PROGRAM_ID
         );
     
-        const presale_ata = anchor.utils.token.associatedAddress({
-          mint: TOKEN_MINT,
-          owner: presalePda,
-        });
-        const staking_ata = anchor.utils.token.associatedAddress({
-          mint: TOKEN_MINT,
-          owner: stakingPda,
-        });
-    
-        const usdc_presale_ata = anchor.utils.token.associatedAddress({
-          mint: USDC_MINT,
-          owner: presalePda,
-        });
-    
-        const usdc_signer_ata = anchor.utils.token.associatedAddress({
-          mint: USDC_MINT,
-          owner: new anchor.web3.PublicKey(
-            "HzmZ5f16agTyCrFFPDi2T7vgpAqfENSLUWLEefH3bpDX"
-          ),
-        });
         console.log("presalePda", presalePda.toString());
         console.log("stakingPda", stakingPda.toString());
-        console.log("presale_ata", presale_ata.toString());
-        console.log("staking_ata", staking_ata.toString());
     
-        console.log("usdc_presale_ata", usdc_presale_ata.toString());
-        console.log("usdc_signer_ata", usdc_signer_ata.toString());
-        
-const startPresaleContext = {
-      signer:wallet.publicKey,
-      presale:presalePda,
-      staking:stakingPda,
-      tokenMint:TOKEN_MINT,
-      usdcMint:USDC_MINT,
-      presaleUsdcAccount:usdc_presale_ata,
-      stakingTokenAccount:staking_ata,
-      presaleTokenAccount:presale_ata,
-      systemProgram: anchor.web3.SystemProgram.programId,
-      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
-      
-    }
+
 
     // Add your test here.
-    const configIx =  await program.methods.initializer(
-      new BN(1741088159),
-      new BN(368664),
-      new BN(79067) 
-    )        
-    .accounts(startPresaleContext)
-    .instruction();
+    const newAuthority = new anchor.web3.PublicKey(
+      "HtcmNSmpM6xGWLH7TcUiyjXQcej32qc15wyzawJYKNMn"
+    );
+    const context = {
+      presale:presalePda,
+      signer:wallet.publicKey,
+    };
 
-            // @ts-ignore
-            // const configIx = await program.methods
-            //     .setConfig(
-            //         tradeFee,
-            //         new anchor.BN(creationFee),
-            //         feeCollector,
-            //         nativeFee,
-            //         initialBuyLimit
-            //     )
-            //     .accounts({
-            //         configAccount,
-            //         user,
-            //         systemProgram: SYSTEM_PROGRAM_ID,
-            //     })
-            //     .instruction();
-
-            // console.log("Config IX:", configIx);
+     const configIx = await program.methods
+      .updatePresaleAuthority(newAuthority)
+      .accounts(context)
+      .instruction();
+     
 
             const tx = new Transaction().add(configIx);
 
@@ -154,10 +104,11 @@ const startPresaleContext = {
             // console.log("Transaction:", tx);
             const signedTx = await wallet.signTransaction(tx);
 
-             // const simulateResult = await connection.simulateTransaction(signedTx);
+            // const simulateResult = await connection.simulateTransaction(signedTx);
             // console.log("Simulate result: ", simulateResult);
             const txId = await sendAndConfirmTransaction(connection, signedTx, [keypair]);
             console.log("txId ", txId);
+
     } catch (error) {
         console.error("Error fetching fee accounts:", error);
     }
